1b2e5fa4f7158f65b696a2d14b35d693d9ce1933
Ugurhan Dasdemir <ugurhandasdemir@gmail.com>
2025-11-21 21:17:42 +0300

    sorguya ollama fonk eklendi

--- sorgu.py
+++ sorgu.py
@@ -1,6 +1,40 @@
 import chromadb
 from chromadb.utils import embedding_functions
 
+
+def ollama(self, question: str) -> str:
+    system = """
+    You are a translator for manga dialogues.
+
+    Rules:
+    1. Translate from English to Turkish.
+    2. Do not translate proper names (manga titles, character names, location names, etc.).
+    3. Fix broken words or lines. Example: "THIS 15 17" → "This is it".
+    4. Remove any "-" characters from the text unless they are part of a proper name.
+    5. Return only the translated text. No explanations, no extra words.
+    """
+    template = """
+    System: {system}
+    Translate this: {question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    model = OllamaLLM(model = "llama3.1:8b")
+
+    chain = prompt | model
+    
+
+    answer = chain.invoke({
+        "system": system,
+        "question": question
+    })
+            
+    return answer
+
+
+
+
 client = chromadb.PersistentClient(path="./yerel_veritabani")
 
 ef = embedding_functions.SentenceTransformerEmbeddingFunction(
